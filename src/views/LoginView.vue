c3740b8e8e6c7e70f89c9d8743f3ce4582f2fd03
Muhammad Ramdhan Aditya S <mramdhanass@gmail.com>
2023-04-03 16:26:50 +0700

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,41 @@
 <template>
-  <v-app class="body">
-   <v-container>
-    <v-card tile elevation="10" class="card-radius" justify="center" align="center">
-      <v-card-title>Login
-      </v-card-title>
-      <v-form v-model="valid">
-      <v-card-text>
-          <v-text-field :rules="userRules" label="Username" prepend-icon="mdi-account-circle" v-model="user.username" />
-          <v-text-field :rules="pwRules" label="Password" :type="showPassword ? 'text' : 'password'"
-           prepend-icon="mdi-lock" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="handleIcon" v-model="user.password" />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="success" @click="handleLogin" :disabled="!valid">Login</v-btn>
-        <v-btn color="info">Register</v-btn>
-      </v-card-actions>
-    </v-form>
-
-    </v-card>
-   </v-container>
+  <v-app class="body">  
+    <v-container>
+      <v-layout wrap style="margin-top: 100px;">
+        <v-flex sm14 md10 offset-md1>
+          <v-card style="height: 466px; border-radius: 20px;">
+            <v-col md="12">
+              <v-row>
+                <v-col md="7" class="login">
+                  <!-- <v-img src="../assets/login.png"
+                         max-height="10000"
+                         max-width="10000"
+                  ></v-img> -->
+                </v-col>
+                <v-col md="5">
+                    <v-img src="../assets/logo-sig.png"
+                           max-height="200"
+                           max-width="500"
+                    ></v-img>
+                    <v-card-title>Masuk ke Pallet Management System
+                    </v-card-title>
+                    <v-form v-model="valid">
+                      <v-card-text>
+                          <v-text-field :rules="userRules" label="Username" prepend-icon="mdi-account-circle" v-model="user.username" />
+                          <v-text-field :rules="pwRules" label="Password" :type="showPassword ? 'text' : 'password'"
+                          prepend-icon="mdi-lock" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="handleIcon" v-model="user.password" />
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn block color="success" @click="handleLogin" :disabled="!valid">Masuk</v-btn>
+                      </v-card-actions>
+                    </v-form>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
