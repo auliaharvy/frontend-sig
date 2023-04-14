e918fa9039aaec75d9edd1552ef220bb502da1e6
Muhammad Ramdhan Aditya S <mramdhanass@gmail.com>
2023-04-14 22:27:03 +0700

    nambah file perfolder

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-flex align-center">
+      <v-breadcrumbs class="ml-auto">
+    <v-breadcrumbs-item class="bcformat" href="/dashboard">{{$t("sidebar.halamanutama")}} ></v-breadcrumbs-item>
+    <v-breadcrumbs-item href="/kategori">Test</v-breadcrumbs-item>
+  </v-breadcrumbs>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Breadcomp',
+  props: {
+    homeLink: {
+      type: String,
+      default: '/dashboard'
+    },
+    homeText: {
+      type: String,
+      default: 'Beranda'
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bcformat {
+    font-size: 12px;
+    color: #fffff;
+}
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import en from "@/locales/en.json"
+import id from "@/locales/id.json"
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const state = {
+  language: localStorage.getItem('language') || 'id',
+  locale: {},
+  languageOptions: [ // Menyimpan pilihan bahasa dalam array
+    { value: 'id', label: 'Indonesia' },
+    { value: 'en', label: 'English' }
+  ],
+  selectedLanguageOption: localStorage.getItem('language') || 'id' // Menyimpan selected item pilihan bahasa
+};
+
+const mutations = {
+  SET_LANGUAGE(state, language) {
+    Vue.set(state, 'language', language); // Menggunakan Vue.set atau $set untuk mengamati perubahan pada state
+    state.locale = require(`../locales/${language}.json`);
+    localStorage.setItem('language', language);
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  SET_SELECTED_LANGUAGE_OPTION(state, languageOption) {
+    Vue.set(state, 'selectedLanguageOption', languageOption); // Menggunakan Vue.set atau $set untuk mengamati perubahan pada state
   }
-})
+};
+
+const actions = {
+  setLanguage({ commit }, language) {
+    commit('SET_LANGUAGE', language);
+  },
+  setSelectedLanguageOption({ commit }, languageOption) {
+    commit('SET_SELECTED_LANGUAGE_OPTION', languageOption);
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions
+});
