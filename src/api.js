ddf5e700d8e65964bdf731bbd859e19f22e1d4d3
harbyanwardi <anwardiharby@gmail.com>
2023-04-18 09:04:05 +0700

    fixing api login

--- src/api.js
+++ src/api.js
@@ -5,6 +5,9 @@ const API_URL = 'http://localhost:3000'
 const apiClient = axios.create({
   baseURL: API_URL,
   headers: {
+    "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
