cd77cc33798c842f88e104eaee3cdfb325384daa
Dzikri Alan <dzikrialan0@gmail.com>
2023-06-27 09:34:15 +0700

    progress print di klaim pallet di jam setengah 10

--- src/views/klaim-pallet/PrintView.vue
+++ src/views/klaim-pallet/PrintView.vue
@@ -123,56 +123,25 @@
   </nav>
     <v-col md-12>
       <v-card class="container-print-section" style="display: flex; justify-content:center; padding-top: 50px; padding-bottom: 50px; background-color: #F5F5F5;">
-        <!-- <v-card style="display: flex; justify-content:center; flex-direction: column; width: 33%; padding: 30px;"> -->
-        <v-card class="print-section" style="display: flex; justify-content:center; flex-direction: column; width: 287px; padding: 15px;">
-          <div style="text-align: center; font-size: 12px; padding-bottom: 18px;">SJP Status Print</div>
-          <div style="text-align: center; font-size: 10px; font-weight: bold; padding-bottom: 14px;"><strong>{{ sjpStatus.sjp_number }}</strong></div>
-          <router-link
-            :to="{ name: 'sjp-status.receive', params: { id: id } }"
-            style="display: flex; justify-content: center;"
-          >
-            <qrcode-vue :value="baseUrl + '/sjp-status/receive/' + this.$route.params.id" :size="100" level="H" />
-          </router-link>
-
-          <div style="text-align: center; font-size: 12px; font-weight: bold; padding-top: 18px;"><strong>Pallet</strong></div>
-            <table style="width: 100%;">
-              <tr style="width: 100%;">
-                <td style="width: 36%; font-size: 12px;">Good Pallet</td>
-                <td style="width: 2%">:</td>
-                <td style="width: 62%; font-size: 12px; text-align: end;">{{ sjpStatus.good_pallet }}</td>
-              </tr>
-              <tr style="width: 100%;">
-                <td style="width: 36%; font-size: 12px;">TBR Pallet</td>
-                <td style="width: 2%">:</td>
-                <td style="width: 62%; font-size: 12px; text-align: end;">{{ sjpStatus.tbr_pallet }}</td>
-              </tr>
-              <tr style="width: 100%;">
-                <td style="width: 36%; font-size: 12px;">BER Pallet</td>
-                <td style="width: 2%">:</td>
-                <td style="width: 62%; font-size: 12px; text-align: end;">{{ sjpStatus.ber_pallet }}</td>
-              </tr>
-            </table>
-          <div style="text-align: center; font-size: 12px; font-weight: bold; padding-top: 18px;"><strong>Pool Pallet</strong></div>
-            <table style="width: 100%;">
-              <tr style="width: 100%;">
-                <td style="width: 36%; font-size: 12px;">Keberangkatan</td>
-                <td style="width: 2%">:</td>
-                <td style="width: 62%; font-size: 12px; text-align: end;">{{ sjpStatus.is_sendback == 1 ? sjpStatus.destination_company : sjpStatus.departure_company }}</td>
-              </tr>
-              <tr style="width: 100%;">
-                <td style="width: 36%; font-size: 12px;">Tujuan</td>
-                <td style="width: 2%">:</td>
-                <td style="width: 62%; font-size: 12px; text-align: end;">{{ sjpStatus.is_sendback == 1 ? sjpStatus.departure_company : sjpStatus.destination_company}}</td>
-              </tr>
-            </table>
-          <div style="text-align: center; font-size: 12px; font-weight: bold; padding-top: 18px;"><strong>Ekspeditur</strong></div>
-          <div style="text-align: center; font-size: 12px;">{{ sjpStatus.transporter_company }}</div>
-          <div style="text-align: center; font-size: 12px; font-weight: bold; padding-top: 18px;"><strong>Note</strong></div>
-          <div style="text-align: center; font-size: 12px;">{{ sjpStatus.note }}</div>
-          <div style="text-align: center; font-size: 12px; font-weight: bold; padding-top: 18px;"><strong>Waktu Pengiriman</strong></div>
-          <div style="text-align: center; font-size: 12px; padding-bottom: 50px;">{{ tanggal }}</div>
-          <div style="display: flex; justify-content: center;">
-            <v-btn class="btn-print" style="width: 110px; font-size: 12px;" onclick="window.print();">PRINT</v-btn>
+        <v-card class="print-section" style="display: flex; justify-content:center; flex-direction: column; width: 75%; padding: 30px;">
+        <!-- <v-card class="print-section" style="display: flex; justify-content:center; flex-direction: column; width: 287px; padding: 15px;"></v-card> -->
+          <div style="text-align: center; font-size: 20px; padding-bottom: 18px;">FAKTUR TAGIHAN</div>
+          <div style="width: 100%;">
+            <div style="display: inline-block; width: 50%;">
+              <div style="font-size: 18px;"><strong>PT. SEMEN INDONESIA</strong></div>
+              <div style="padding-bottom: 15px;">
+                <div style="font-size: 14px;">Jl. Jendral Sudirman No.00</div>
+                <div style="font-size: 14px;">Jakarta Utara 14330</div>
+              </div>
+              <div>
+                <div style="font-size: 14px;">Telp. 102390124090</div>
+                <div style="font-size: 14px;">Fax. 21318024938</div>
+              </div>
+            </div>
+            <div style="display: inline-block; width: 50%;">
+              <div style="font-size: 18px;"><strong>{{ claimPallet.trx_number }}</strong></div>
+              <qrcode-vue :value="baseUrl + '/sjp-status/receive/' + this.$route.params.id" :size="100" level="H" />
+            </div>
           </div>
         </v-card>
       </v-card>
